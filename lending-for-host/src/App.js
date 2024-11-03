925db53d7dcbfcb99f1c3c698766b85c8df50da8
AlexTrish <96265323+AlexTrish@users.noreply.github.com>
2024-11-03 16:44:39 +0300

    v0.0.4

--- lending-for-host/src/App.js
+++ lending-for-host/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Header from './components/js/Header';
+import React, { useState } from 'react';
 import Dashboard from './components/js/Dashboard';
+import Header from './components/js/Header';
 import Footer from './components/js/Footer';
+import './components/css/styles.scss';
 
 function App() {
+    const [currentMenu, setCurrentMenu] = useState('Main');
+
     return (
         <div className="app">
-            <Header />
-            <Dashboard />
+            <Header setCurrentMenu={setCurrentMenu} />
+            <Dashboard currentMenu={currentMenu} setCurrentMenu={setCurrentMenu} />
             <Footer />
         </div>
     );
--- lending-for-host/src/components/js/Dashboard.js
+++ lending-for-host/src/components/js/Dashboard.js
@@ -1,57 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Content_main from './Content_main';
 import Content_1 from './Content_1';
 import Content_2 from './Content_2';
 import Content_3 from './Content_3';
 import Content_4 from './Content_4';
 
-function Dashboard() {
+import '../css/styles.scss';
 
-
-useEffect(() => {
+function Dashboard({ currentMenu }) {
+  useEffect(() => {
     localStorage.setItem('currentMenu', currentMenu);
-}, [currentMenu]);
+  }, [currentMenu]);
 
-
-
-    return (
-        <div className="dashboard">
-            {currentMenu === 'Main' ? (
-                <div>
-                    <Content_main />
-                    <Content_1 />
-                    <Content_2 />
-                    <Content_3 />
-                    <Content_4 />
-                </div>
-            ) : currentMenu === 'VPS' ? (
-                <div>
-                    
-                </div>
-            ) : currentMenu === 'VDS' ? (
-                <div>
-
-                </div>
-            ) : currentMenu === 'VPN' ? (
-                <div>
-
-                </div>
-            ) : currentMenu === 'cl-pn' ? (
-                <div>
-
-                </div>
-            ) : currentMenu === 'bulletproof' ? (
-                <div>
-
-                </div>
-            ) : currentMenu === 'VPS' ? (
-                <div>
-
-                </div>
-            ) : (
-            )}
+  return (
+    <div className="dashboard">
+      {currentMenu === 'Main' ? (
+        <div>
+          <Content_main />
+          <Content_1 />
+          <Content_2 />
+          <Content_3 />
+          <Content_4 />
         </div>
-    );
+      ) : currentMenu === 'VPS' ? (
+        <div> <Content_1 /> {/* Контент для VPS */} </div>
+      ) : currentMenu === 'VDS' ? (
+        <div> {/* Контент для VDS */} </div>
+      ) : currentMenu === 'VPN' ? (
+        <div> {/* Контент для VPN */} </div>
+      ) : currentMenu === 'cl-pn' ? (
+        <div> {/* Контент для Cloud-Panel */} </div>
+      ) : currentMenu === 'bulletproof' ? (
+        <div> {/* Контент для Bulletproof */} </div>
+      ) : currentMenu === 'domain' ? (
+        <div> {/* Контент для domain */} </div>
+      ) : currentMenu === 'buy-domain' ? (
+        <div> {/* Контент для buy-domain */} </div>
+      ) : currentMenu === 'rules' ? (
+        <div> {/* Контент для rules */} </div>
+      ) : currentMenu === 'referal' ? (
+        <div> {/* Контент для referal */} </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default Dashboard;
